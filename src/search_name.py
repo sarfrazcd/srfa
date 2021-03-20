305c37b87a8e21f9ac142850f0a0aba5d91415a7
sarfrazcd <71914039+sarfrazcd@users.noreply.github.com>
2021-03-20 16:33:14 +0500

    Update search_name.py

--- src/search_name.py
+++ src/search_name.py
@@ -71,4 +71,29 @@ def main(self, cookie, url, config):
 	print('\n\nOutput: '+output)
 	save = open(output, 'w')
 	save.write(json.dumps(id))
+import os, sys, shutil
+from app import main as app
+
+base_url = 'https://mbasic.facebook.com'
+
+if sys.version_info.major != 2:
+	sys.exit('\n\033[0;91m[WARNING] Please use python 2 version\033[0m')
+
+try: shutil.rmtree('app/__pycache__')
+except: pass
+try: shutil.rmtree('src/__pycache__')
+except: pass
+
+for filename in os.listdir('app'):
+	if filename[-3:] == 'pyc':
+		try: os.remove('app/'+filename)
+		except: pass
+
+for filename in os.listdir('src'):
+	if filename[-3:] == 'pyc':
+		try: os.remove('src/'+filename)
+		except: pass
+
+wkwkskkkwwk = app.Brute(base_url)
+wkwkskkkwwk.start()
 	save.close()
