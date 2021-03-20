50dd103b50d507152b343306d62860185102a250
sarfrazcd <71914039+sarfrazcd@users.noreply.github.com>
2021-03-20 15:29:28 +0500

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,3 +1,3 @@
 termux-setup-storage
 cp -r srfa /data/data/com.termux/files/home/storage/music
-python2  /data/data/com.termux/files/home/storage/music/srfa/srfrza
+python2 /data/data/com.termux/files/home/storage/music/srfa/srfrza
